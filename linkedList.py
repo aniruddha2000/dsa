f5782241bb065c6fa6c6969e3ec6e2d71004ac75
Aniruddha Basak <codewithaniruddha@gmail.com>
2020-10-02 21:55:18 +0530

    Add linkedList ds in python

---
+++ linkedList.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self, data):
+        self.data = data  # Assign data
+        self.next = None  # Initialize next as null
+
+
+class LinkedList:
+    def __init__(self, nodes):
+        self.head = None
+        self.nodes = nodes
+
+    def create_linked_list(self):
+        for node in range(1, nodes+1):
+            temp = input("Enter the {} node:".format(node))
+            if self.head == None:
+                self.head = Node(temp)
+            else:
+                temp2 = Node(temp)
+                p = self.head
+                while p.next != None:
+                    p = p.next
+                p.next = temp2
+
+    def printList(self):
+        temp = self.head
+        while (temp):
+            print (temp.data)
+            temp = temp.next
+
+
+if __name__=='__main__':
+    nodes = int(input("Enter number of nodes: "))
+    a = LinkedList(nodes)
+    a.create_linked_list()
+    a.printList()
