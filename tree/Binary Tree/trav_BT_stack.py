668355364b8e65fdf1570999478aa9d9bd9969bd
Aniruddha Basak <codewithaniruddha@gmail.com>
2020-10-13 21:08:14 +0530

    Trav binary tree post order with stack

--- tree/Binary Tree/trav_BT_stack.py
+++ tree/Binary Tree/trav_BT_stack.py
@@ -29,6 +29,30 @@ def inorder_trav(root):
         print(root.val, end=" ")
         root = root.right
 
+def peek(stack):
+    if len(stack) > 0:
+        return stack[-1]
+    return None
+
+def postorder_trav(root):
+    stack = []
+    while True:
+        while root:
+            if root.right is not None:
+                stack.append(root.right)
+            stack.append(root)
+            root = root.left
+        if len(stack) == 0:
+            break
+        root = stack.pop()
+        if (root.right is not None and peek(stack) == root.right):
+            stack.pop()
+            stack.append(root)
+            root = root.right
+        else:
+            print(root.val, end=" ")
+            root = None
+
 
 root = Node(1)
 root.left = Node(2)
@@ -38,5 +62,5 @@ root.left.right = Node(5)
 root.right.left = Node(6)
 root.right.right = Node(7)
 # preorder_trav(root)
-inorder_trav(root)
+postorder_trav(root)
 print()
