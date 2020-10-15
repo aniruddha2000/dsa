02d1dc1929739b3097bb349119094cb585efe313
Aniruddha Basak <codewithaniruddha@gmail.com>
2020-10-15 21:50:03 +0530

    Delete binary tree

---
+++ tree/Binary Tree/deleting/level_order.py
@@ -0,0 +1,31 @@
+from queue import Queue
+
+
+class Node:
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.val = data
+
+
+def del_node(root):
+    queue = Queue()
+    queue.put(root)
+    while queue.qsize() > 0:
+        temp_node = queue.get()
+        if temp_node.left is not None:
+            queue.put(temp_node.left)
+        if temp_node.right is not None:
+            queue.put(temp_node.right)
+        temp_node = None
+    root = None
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+root.right.left = Node(6)
+root.right.right = Node(7)
+del_node(root)
+print()
