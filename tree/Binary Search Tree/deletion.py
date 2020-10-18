ced2437835b9e5244c1d55c287d076d6b0de1094
Aniruddha Basak <codewithaniruddha@gmail.com>
2020-10-18 12:54:27 +0530

    Temp

--- tree/Binary Search Tree/deletion.py
+++ tree/Binary Search Tree/deletion.py
@@ -5,6 +5,15 @@ class Node:
         self.right = None
 
 
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    if key < root.val:
+        root.left = insert(root.left, key)
+    else:
+        root.right = insert(root.right, key)
+    return root
+
 def inorder_trav(root):
     if root:
         inorder_trav(root.left)
@@ -12,13 +21,13 @@ def inorder_trav(root):
         inorder_trav(root.right)
 
 def find_inorder_succ(root):
-    while root.left is not None:
+    while root.left:
         root = root.left
     return root
 
 def delete_node(root, data):
-    if root == None:
-        return "Empty tree"
+    # if root == None:
+    #     return "Empty tree"
     parent = None
     curr = root
     while curr and curr.val != data:
@@ -27,41 +36,53 @@ def delete_node(root, data):
             curr = curr.left
         else:
             curr = curr.right
+    if curr is None:
+        return root
+
     # case 1(node has no child):
     if curr.left == None and curr.right == None:
-        if parent.left == curr:
-            parent.left = None
+        if curr != root:
+            if parent.left == curr:
+                parent.left = None
+            else:
+                parent.right = None
         else:
-            parent.right = None
+            root = None
+
+    # case 3(node has both child):
+    elif curr.left and curr.right:
+        inorder_succ = find_inorder_succ(curr.right)
+        curr.val = inorder_succ.val
+        delete_node(root, inorder_succ.val)
 
     # case 2(node has one child):
-    elif curr.left != None or curr.right != None:
+    else:
         if curr.left:
             child = curr.left
         else:
             child = curr.right
 
-        if curr == parent.left:
-            parent.left = child
-            curr = None
-            child = None
+        if curr != root:
+            if curr == parent.left:
+                parent.left = child
+                curr = None
+                child = None
+            else:
+                parent.right = child
+                curr = None
+                child = None
         else:
-            parent.right = child
-            curr = None
-            child = None
+            root = child
     return root
 
 
-root = Node(8)
-root.left = Node(3)
-root.right = Node(10)
-root.left.left = Node(1)
-root.left.right = Node(6)
-root.left.right.left = Node(5)
-root.right.left = Node(9)
-root.right.right = Node(14)
-root.right.right.right = Node(17)
+root = None
+keys = [8, 10, 3, 1, 6, 5, 9, 14, 12, 17]
+
+for key in keys:
+    root = insert(root, key)
 inorder_trav(root)
 print()
-root = delete_node(root, 6)
+root = delete_node(root, 10)
 inorder_trav(root)
+print()
