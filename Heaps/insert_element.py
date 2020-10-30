a19c2fdff0c68104156424e5fb978acaffb4731d
Aniruddha Basak <codewithaniruddha@gmail.com>
2020-10-30 21:52:12 +0530

    Insert element in heap

---
+++ Heaps/insert_element.py
@@ -0,0 +1,22 @@
+from Build_array import printHeap
+
+
+def heapify(arr, n, i):
+    parent = (i-1)//2
+    if arr[parent] > 0:
+        if arr[i] > arr[parent]:
+            arr[i], arr[parent] = arr[parent], arr[i]
+            heapify(arr, n, parent)
+
+def insert_ele(arr, n, data):
+    n = n + 1
+    arr.append(data)
+    heapify(arr, n, n-1)
+    return arr
+
+
+arr = [10, 5, 3, 2, 4]
+n = len(arr)
+arr = insert_ele(arr, n, 15)
+n = len(arr)
+printHeap(arr, n)
