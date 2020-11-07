6a4b787dac3abf6fe09ec0c2fa0bd7786dc5a006
Aniruddha Basak <codewithaniruddha@gmail.com>
2020-11-07 13:20:47 +0530

    Largest Rectangle problem hackerrank

---
+++ stacks/largest_rectangle.py
@@ -0,0 +1,16 @@
+def largestRectangle(h):
+    stack = []
+    area = i = 0
+    h.append(0)
+    while i < len(h):
+        if not stack or h[stack[-1]] < h[i]:
+            stack.append(i)
+            i += 1
+        else:
+            top = stack.pop()
+            area = max(area, h[top] * (i - stack[-1] - 1 if stack else i))
+    return area
+
+
+h = [1, 2, 3, 4, 5]
+print(largestRectangle(h))
