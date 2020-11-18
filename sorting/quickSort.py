7a4ed34d74310e21812cf41441f104c913e00404
Aniruddha Basak <codewithaniruddha@gmail.com>
2020-11-18 12:01:21 +0530

    Quick sort solve

--- sorting/quickSort.py
+++ sorting/quickSort.py
@@ -4,18 +4,15 @@ def partition(arr, low, high):
     for j in range(low, high):
         if arr[j] < pivot:
             i += 1
-            arr[i], arr[j], arr[j], arr[i]
+            arr[i], arr[j] = arr[j], arr[i]
     arr[i+1], arr[high] = arr[high], arr[i+1]
     return i+1
-
 
 def quickSort(arr, low, high):
     if low < high:
         pi = partition(arr,low,high)
-        
         quickSort(arr, low, pi-1)
         quickSort(arr, pi+1, high)
-
 
 arr = [10, 7, 8, 9, 1, 5]
 n = len(arr)
