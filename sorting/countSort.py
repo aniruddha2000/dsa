42836edbb73ab2957f40c2e43cb53b7ffe7bb97b
Aniruddha Basak <codewithaniruddha@gmail.com>
2020-11-19 14:55:03 +0530

    Add count sort solve

---
+++ sorting/countSort.py
@@ -0,0 +1,26 @@
+def count_sort(arr):
+    max_ele = int(max(arr))
+    min_ele = int(min(arr))
+
+    count = [0 for i in range(max_ele+1)]
+    output = [0 for i in range(len(arr))]
+
+    for i in arr:
+        count[i] += 1
+
+    i, j = 0, 0
+    while i < max_ele+1:
+        if count[i] > 0:
+            arr[j] = i
+            j += 1
+            count[i] -= 1
+        i += 1
+
+
+arr = [5, 6, 2, 9, 3, 7, 1]
+count_sort(arr)
+
+print ("Sorted array is:")
+for i in range(len(arr)):
+    print ("%d" %arr[i], end=" ")
+print()
