06eeefa9e82bb1ef05179d621bb66aebe53b9be6
Aniruddha Basak <codewithaniruddha@gmail.com>
2020-11-21 13:35:12 +0530

    Update roads and libraries

--- Graph/HR/Roads_Libraries.py
+++ Graph/HR/Roads_Libraries.py
@@ -1,6 +1,8 @@
 from collections import defaultdict
 
 def DFS(n, visited, nodes, graph):
+    if visited[n] == True:
+        return 0
     nodes += 1
     visited[n] = True
     for i in graph[n]:
@@ -16,9 +18,9 @@ def roadsAndLibraries(n, c_lib, c_road, cities):
         graph[j].append(i)
 
     cost = 0
-    nodes = 0
     for i in range(1, n+1):
         if visited[i] == False:
+            nodes = 0
             nodes = DFS(i, visited, nodes, graph)
             cost = cost + c_lib
             if c_lib > c_road:
@@ -28,5 +30,5 @@ def roadsAndLibraries(n, c_lib, c_road, cities):
     print(cost)
 
 
-cities = [[1, 2], [3, 1], [2, 3]]
-roadsAndLibraries(3, 2, 1, cities)
+cities = [[1, 3], [3, 4], [2, 4], [1, 2], [2, 3], [5, 6]]
+roadsAndLibraries(6, 2, 5, cities)
