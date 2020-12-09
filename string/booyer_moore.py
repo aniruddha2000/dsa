cdfb86d64656e8b150ac21c79f2ebc9333219230
Aniruddha Basak <codewithaniruddha@gmail.com>
2020-12-09 18:08:18 +0530

    Add booyer_moore string search

---
+++ string/booyer_moore.py
@@ -0,0 +1,28 @@
+NO_OF_CHARS = 256
+
+def badCharHeuristic(pat, m):
+    badChar = [-1]*NO_OF_CHARS
+    for i in range(m):
+        badChar[ord(pat[i])] = i
+    return badChar
+
+def search(txt, pat):
+    m = len(pat)
+    n = len(txt)
+
+    badChar = badCharHeuristic(pat, m)
+    s = 0
+    while s <= n-m:
+        j = m -1
+        while j>=0 and pat[j] == txt[s+j]:
+            j -= 1
+        if j<0:
+            print("Pattern occur at shift = {}".format(s))
+            s += (m-badChar[ord(txt[s+m])] if s+m<n else 1)
+        else:
+            s += max(1, j-badChar[ord(txt[s+j])])
+
+
+txt = "ABAAABCD"
+pat = "ABC"
+search(txt, pat)
