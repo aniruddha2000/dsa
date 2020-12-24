8c4dfef0d3cc8998ca18a8d9eef5a2eb7a1fc63f
Aniruddha Basak <codewithaniruddha@gmail.com>
2020-12-24 22:19:08 +0530

    Add bottom up dynamic programming

---
+++ DP/bottom_up.py
@@ -0,0 +1,12 @@
+def fib_bottom_up(n):
+    if n == 1 or n == 2:
+        return 1
+    bottom_up = [None] * (n+1)
+    bottom_up[1] = 1
+    bottom_up[2] = 2
+    for i in range(3, n+1):
+        bottom_up[i] = bottom_up[i-1] + bottom_up[i-2]
+    return bottom_up[n]
+
+n = int(input())
+print(fib_bottom_up(n))
